c83c4bcf61edeb4a1de3d6e164e489b8af6fec87
RavicharanN <ravicharan@Ravis-MacBook-Pro.local>
2025-02-26 14:13:40 -0500

    Add trition related scripts for resnet18 testing

---
+++ triton/check_inference_server_up.py
@@ -0,0 +1,6 @@
+import tritonclient.http as httpclient
+
+client = httpclient.InferenceServerClient(url="localhost:8000")
+model_metadata = client.get_model_metadata("resnet18")
+print(model_metadata)
+
